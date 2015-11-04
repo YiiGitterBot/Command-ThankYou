b3d8578e73467856cc7be969c2c40222eeef7627
Alex Bond <alex.bondarevskiy@gmail.com>
2015-11-04 01:25:44 -0800

    1.1 - Multi room support

--- src/main/java/org/YiiCommunity/GitterBot/ThankYouCommand/ThankYou.java
+++ src/main/java/org/YiiCommunity/GitterBot/ThankYouCommand/ThankYou.java
@@ -2,6 +2,7 @@ package org.YiiCommunity.GitterBot.ThankYouCommand;
 
 import com.amatkivskiy.gitter.rx.sdk.model.response.message.MessageResponse;
 import com.amatkivskiy.gitter.rx.sdk.model.response.room.Mention;
+import com.amatkivskiy.gitter.rx.sdk.model.response.room.RoomResponse;
 import org.YiiCommunity.GitterBot.api.Command;
 import org.YiiCommunity.GitterBot.containers.Gitter;
 import org.YiiCommunity.GitterBot.models.database.CarmaHistory;
@@ -19,7 +20,7 @@ public class ThankYou extends Command {
     }
 
     @Override
-    public void onMessage(MessageResponse message) {
+    public void onMessage(RoomResponse room, MessageResponse message) {
         if (message.mentions.isEmpty())
             return;
 
@@ -30,7 +31,7 @@ public class ThankYou extends Command {
                 if (text.startsWith(item) || text.endsWith(item)) {
                     User giver = User.getUser(message.fromUser.username);
 
-                    ArrayList<String> sent = new ArrayList<String>();
+                    ArrayList<String> sent = new ArrayList<>();
                     for (Mention mention : message.mentions) {
                         if (sent.contains(mention.screenName))
                             continue;
@@ -38,13 +39,13 @@ public class ThankYou extends Command {
                         sent.add(mention.screenName);
 
                         if (mention.screenName.equals(message.fromUser.username)) {
-                            Gitter.sendMessage(getConfig().getString("messages.selfThanks", "*@{username} selflike? How vulgar!*").replace("{username}", message.fromUser.username));
+                            Gitter.sendMessage(room, getConfig().getString("messages.selfThanks", "*@{username} selflike? How vulgar!*").replace("{username}", message.fromUser.username));
                             continue;
                         }
                         User receiver = User.getUser(mention.screenName);
 
                         if (System.currentTimeMillis() / 1000 - CarmaHistory.getLastThankYou(giver, receiver) < getConfig().getInt("minTimeBetweenThanks", 60)) {
-                            Gitter.sendMessage(
+                            Gitter.sendMessage(room,
                                     getConfig()
                                             .getString("messages.tooSoon", "You tried to change @{receiver} carma too often. Canceling.")
                                             .replace("{receiver}", receiver.getUsername())
@@ -55,15 +56,15 @@ public class ThankYou extends Command {
 
 
                         receiver.changeCarma(1, giver, message.text);
-                        Gitter.sendMessage(
+                        Gitter.sendMessage(room,
                                 getConfig()
                                         .getString("messages.thanks", "*Thanks (+1) to @{receiver} accepted! Now his carma **{carma}**.*")
                                         .replace("{receiver}", receiver.getUsername())
                                         .replace("{giver}", message.fromUser.username)
                                         .replace("{carma}", (receiver.getCarma() >= 0 ? "+" : "-") + receiver.getCarma())
                         );
-                        receiver.updateAchievements();
-                        giver.updateAchievements();
+                        receiver.updateAchievements(room);
+                        giver.updateAchievements(room);
                     }
                     break;
                 }
